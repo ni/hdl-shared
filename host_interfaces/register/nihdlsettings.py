e245ab7c48b8fc9fd27c8940c0cb8ac0be95a5a4
Salvador Santoluctio <salvador.santolucito@ni.com>
2026-05-09 15:24:35 -0500

    refactor settings

--- host_interfaces/register/nihdlsettings.py
+++ host_interfaces/register/nihdlsettings.py
@@ -6,13 +6,14 @@ def pre_all(context):
     config = context.config
 
     # --- Tools ---
-    config.set_vivado_tools_path("C:/NIFPGA/programs/Vivado2021_1")
+    config.set_vivado_tools_folder("C:/NIFPGA/programs/Vivado2021_1")
     config.set_vivado_tcl_scripts_folder("../common/TCL")
 
     # --- Vivado Project Settings ---
     config.set_top_level_entity("tb_NiSharedHostRegister")
     config.set_fpga_part("xcku040-ffva1156-2-e")
-    config.set_vivado_project_path("VivadoProject/HdlRegister.xpr")
+    config.set_vivado_project_folder("VivadoProject")
 
+    # --- HDL Source Code ---
     config.add_hdl_file_list("vivadoprojectsources.txt")
     config.add_hdl_file_list("vivadoprojectdeps.txt")
